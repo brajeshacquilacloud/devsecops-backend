68a40c8bf992d8a7717e97e548c1896cfe2d76b7
Shubham Kale <ShubhamKale54@gmail.com>
2022-04-06 17:21:17 +0530

    Y axis Formatting

--- app.js
+++ app.js
@@ -3871,9 +3871,9 @@ app.post('/api/linechart', function (req, res) {
     addDelay();
 
     const data = [
-        [new Date(2014, 0),  -.5,  5.7],
-        [new Date(2014, 1),   .4,  8.7],
-        [new Date(2014, 2),   .5,   12],
+        [new Date(2014, 0),  1.5,  5.7],
+        [new Date(2014, 1),   2.4,  8.7],
+        [new Date(2014, 2),   7.5,   12],
         [new Date(2014, 3),  2.9, 15.3],
         [new Date(2014, 4),  6.3, 18.6],
         [new Date(2014, 5),    9, 20.9],
@@ -3881,8 +3881,8 @@ app.post('/api/linechart', function (req, res) {
         [new Date(2014, 7), 10.3, 16.6],
         [new Date(2014, 8),  7.4, 13.3],
         [new Date(2014, 9),  4.4,  9.9],
-        [new Date(2014, 10), 1.1,  6.6],
-        [new Date(2014, 11), -.2,  4.5]
+        [new Date(2014, 10), 15.1,  6.6],
+        [new Date(2014, 11), 12.2,  4.5]
     ];
 
     setResponseHeaders(res);
